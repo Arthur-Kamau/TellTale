f2d2ad384f7046d2cde9cbbf0007ead773962e20
Arthur-Kamau <kennkamau09@gmail.com>
2021-01-04 01:04:43 +0300

    basics

--- common/src/commonMain/kotlin/org/araizen/teleril/common/App.kt
+++ common/src/commonMain/kotlin/org/araizen/teleril/common/App.kt
@@ -1,18 +1,51 @@
 package org.araizen.teleril.common
-import androidx.compose.material.Text
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.*
-
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 @Composable
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+//    var text by remember { mutableStateOf("Hello, World!") }
+//
+//    MaterialTheme {
+//        Button(onClick = {
+//            text = "Hello, ${getPlatformName()}"
+//        }) {
+//            Text(text)
+//        }
+//    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, ${getPlatformName()}"
-        }) {
-            Text(text)
-        }
-    }
+                title = {
+                    Text(text = "image here")
+//                    Image(
+                      //  imageResource(id = R.drawable.hotspot),
+//                        androidx.compose.ui.graphics.vector.ImageVector.Builder(),
+//                        modifier = Modifier.width(50.dp).height(50.dp).padding(horizontal = 1.dp),
+//                        contentScale = ContentScale.Crop
+//                    )
+                },
+
+
+                backgroundColor = androidx.compose.ui.graphics.Color.White, //colorResource(R.color.material_blue_grey_800),//Color.DarkGray,
+                contentColor = Color.White,
+                elevation = 12.dp
+            )
+        }, bodyContent = {
+
+            Row{
+                Text("Image here")
+            }
+        })
 }
